728c24b4e69107b7482448d82310c93a049db32a
sigireddy viswesh <98519767+Viswesh934@users.noreply.github.com>
2025-07-22 18:02:20 +0530

    Update env.ts

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 import "dotenv/config";
 
 const envSchema = z.object({
-  DATABASE_URL: z.string().default("postgres://postgres:viswesh@localhost:5432/Test"),
+  DATABASE_URL: z.string().default("postgres://postgres:password@localhost:5432/Test"),
   PORT: z.coerce.number().default(3000),
   HOST: z.string().default("127.0.0.1"),
 });
